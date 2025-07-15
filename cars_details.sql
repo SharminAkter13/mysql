927f9b9e8acd5d689bc0c24cc01ae43c91d62274
SharminAkter13 <sharminakter1309200@gmail.com>
2025-07-15 12:45:05 +0600

    new

---
+++ cars_details.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 15, 2025 at 08:43 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cars`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cars_details`
+--
+
+CREATE TABLE `cars_details` (
+  `id` int(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `model` varchar(50) NOT NULL,
+  `brand` varchar(50) NOT NULL,
+  `color` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cars_details`
+--
+
+INSERT INTO `cars_details` (`id`, `name`, `model`, `brand`, `color`) VALUES
+(1, 'Mustang GT', '2023 GT', 'Ford', 'Red'),
+(2, 'Civic Sport', '2022 Sport', 'Honda', 'Blue'),
+(3, 'Model S', '2024 Long Range', 'Tesla', 'White'),
+(4, 'Camry XSE', '2023 XSE', 'Toyota', 'Black');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cars_details`
+--
+ALTER TABLE `cars_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cars_details`
+--
+ALTER TABLE `cars_details`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
