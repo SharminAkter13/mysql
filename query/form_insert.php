995cd1b1e82dc5e23b01936affd8787417ea8cbf
SharminAkter13 <sharminakter13092000@gmail.com>
2025-07-22 11:13:05 +0600

    sql

---
+++ query/form_insert.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        fieldset{
+            border: 1px solid black !important;
+            padding: 30px !important;
+            border-radius: 10px !important;
+        }
+    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+
+</head>
+<body>
+        <h2 style="text-align: center; color:black;">Insert Products Profit </h2>
+    <div class="container mt-5">
+        <form action="">
+            <fieldset>
+                <div class="mb-3">
+                <label for="formGroupExampleInput" class="form-label">Products</label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Enter Product Name">
+                </div>
+                <div class="mb-3">
+                <label for="formGroupExampleInput2" class="form-label">Sales Price</label>
+                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Enter Sales Price">
+                </div>
+                <div class="mb-3">
+                <label for="formGroupExampleInput2" class="form-label">Purchase Price</label>
+                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Enter Purchase Price">
+                </div>
+                <div class="mb-3">
+                <label for="formGroupExampleInput2" class="form-label">Unit</label>
+                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Total Unit">
+                </div>
+                <div class="mb-3">
+                <label for="formGroupExampleInput2" class="form-label">Profit</label>
+                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Profit">
+                </div>
+            </fieldset>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+
+</body>
+</html>
