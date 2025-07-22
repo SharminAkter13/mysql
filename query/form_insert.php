8fcd8b16f68eb7c012b0f2279097f802c64a3a3c
SharminAkter13 <sharminakter13092000@gmail.com>
2025-07-22 12:22:16 +0600

    sql

--- query/form_insert.php
+++ query/form_insert.php
@@ -9,7 +9,7 @@ if(isset($_POST['submit'])){
     $p = $_POST['pprofit'];
 
 
-    $query ="INSERT INTO net_profit(product,sales_price,purchase_price,unit,profit) VALUES($pr,$s, $pp, $u,$p)";
+    $query ="INSERT INTO net_profit(products,sales_price,purchase_price,unit,profit) VALUES('$pr','$s', '$pp', '$u','$p')";
     if(mysqli_query($profit_db,$query) == TRUE){
         echo "DATA SUBMITED";
         header('location:table_mysql.php');
@@ -38,9 +38,9 @@ if(isset($_POST['submit'])){
 
 </head>
 <body>
-        <h2 style="text-align: center; color:black;">Insert Products Profit </h2>
+        <h2 style="text-align: center; color:black;">Insert Products Details </h2>
     <div class="container mt-5">
-        <form action="">
+        <form action="" method="post">
             <fieldset>
                 <div class="mb-3">
                 <label for="formGroupExampleInput" class="form-label">Products</label>
@@ -68,9 +68,7 @@ if(isset($_POST['submit'])){
             </fieldset>
         </form>
     </div>
-    <div>
-    <a href="form_insert.php"><button>Submit Data </button></a>
-    </div>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 
